8f587809ecde484aed672668a0de867fbdf33308
herocharge <techwithbhargav@gmail.com>
2025-04-10 09:37:57 +0530

    [It-2] added chash and redis

---
+++ pkg/chash/ring.go
@@ -0,0 +1,74 @@
+package chash
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"sort"
+	"sync"
+
+	"no.cap/goddb/pkg/rpc"
+)
+
+type Ring struct {
+	mu        sync.RWMutex
+	hashes    []string
+	nodeMetas map[string]rpc.NodeMeta
+}
+
+func NewRing() *Ring {
+	return &Ring{
+		hashes:    []string{},
+		nodeMetas: make(map[string]rpc.NodeMeta),
+	}
+}
+
+func (r *Ring) AddNode(node rpc.NodeMeta) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	nodeID := fmt.Sprintf("%s:%d", node.Ip, node.Port)
+	hash := hashString(nodeID)
+	r.hashes = append(r.hashes, hash)
+	sort.Strings(r.hashes)
+	r.nodeMetas[hash] = node
+}
+
+func (r *Ring) RemoveNode(nodeID string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	hash := hashString(nodeID)
+	for i, h := range r.hashes {
+		if h == hash {
+			r.hashes = append(r.hashes[:i], r.hashes[i+1:]...)
+			break
+		}
+	}
+	delete(r.nodeMetas, hash)
+}
+
+func (r *Ring) GetNode(key string) (string, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	if len(r.hashes) == 0 {
+		return "", errors.New("ring is empty")
+	}
+
+	keyHash := hashString(key)
+	index := sort.SearchStrings(r.hashes, keyHash)
+	if index == len(r.hashes) {
+		index = 0
+	}
+	return r.hashes[index], nil
+}
+
+func hashString(s string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(s))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// TODO: do multiple hash functions?
---
+++ pkg/storage/redis.go
@@ -0,0 +1,68 @@
+package storage
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Store struct {
+	client *redis.Client
+}
+
+type Data struct {
+	Value     []byte    `json:"value"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func NewStore(addr string) (*Store, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to redis: %w", err)
+	}
+
+	return &Store{client: client}, nil
+}
+
+func (s *Store) Put(ctx context.Context, key string, value []byte, timestamp time.Time) error {
+	data := Data{
+		Value:     value,
+		Timestamp: timestamp,
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("failed to marshal data: %w", err)
+	}
+
+	err = s.client.HSet(ctx, key, "data", jsonData).Err()
+	if err != nil {
+		return fmt.Errorf("failed to set key: %w", err)
+	}
+	return nil
+}
+
+func (s *Store) Get(ctx context.Context, key string) (value []byte, timestamp time.Time, err error) {
+	jsonData, err := s.client.HGet(ctx, key, "data").Result()
+	if err == redis.Nil {
+		return nil, time.Time{}, fmt.Errorf("key not found: %w", err)
+	} else if err != nil {
+		return nil, time.Time{}, fmt.Errorf("failed to get key: %w", err)
+	}
+
+	var data Data
+	err = json.Unmarshal([]byte(jsonData), &data)
+	if err != nil {
+		return nil, time.Time{}, fmt.Errorf("failed to unmarshal data: %w", err)
+	}
+
+	return data.Value, data.Timestamp, nil
+}
